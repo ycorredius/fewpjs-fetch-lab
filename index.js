34e4f046f992dc41b8eeef2d93bfc8576d980973
shaun <ycorredius@gmail.com>
2020-02-22 14:55:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(json => renderBooks(json));
 }
 
 function renderBooks(json) {
